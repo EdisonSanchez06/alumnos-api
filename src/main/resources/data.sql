da1aaf94a50178a80eeac6a9eda86e263493d8ce
EdisonSanchez06 <150718088+EdisonSanchez06@users.noreply.github.com>
2025-12-04 14:28:24 -0500

    Agregar usuarios por defecto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuario(username, password, role)
+VALUES ('admin', 'admin123', 'ADMIN');
+
+INSERT INTO usuario(username, password, role)
+VALUES ('secretaria', 'secre123', 'SECRETARIA');
