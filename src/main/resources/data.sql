0e3faed15eca013a37e59e83a07258b9ab6c445c
EdisonSanchez06 <150718088+EdisonSanchez06@users.noreply.github.com>
2025-12-04 14:52:56 -0500

    fix properties and sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO usuario(username, password, role)
-VALUES ('admin', 'admin123', 'ADMIN');
+VALUES ('admin', '$2a$10$5b0fKwY4AqiX9yY0Yiyl4eCq8KeJGjYuDmjq98/WYq8V9ydBmw9CO', 'ADMIN');
+
 
 INSERT INTO usuario(username, password, role)
-VALUES ('secretaria', 'secre123', 'SECRETARIA');
+VALUES ('secretaria', '$2a$10$uI/yrpUIvUUCMNUfpfe1BeUrun6r8JvGv/d5Wuzp23sJYI0qBo5wC', 'SECRETARIA');
