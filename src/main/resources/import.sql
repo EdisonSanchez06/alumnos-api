483e60bde9dfd3d3de43aa6fdc7ece227b1fb618
EdisonSanchez06 <150718088+EdisonSanchez06@users.noreply.github.com>
2025-12-04 22:30:24 -0500

    cambio de properties a postgres

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO usuario(username, password, role)
-VALUES ('admin', '$2a$10$5b0fKwY4AqiX9yY0Yiyl4eCq8KeJGjYuDmjq98/WYq8V9ydBmw9CO', 'ADMIN');
-
-
-INSERT INTO usuario(username, password, role)
-VALUES ('secretaria', '$2a$10$uI/yrpUIvUUCMNUfpfe1BeUrun6r8JvGv/d5Wuzp23sJYI0qBo5wC', 'SECRETARIA');
