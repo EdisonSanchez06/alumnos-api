9ff504e169784033c83b66aba97be532eb78cd4f
EdisonSanchez06 <150718088+EdisonSanchez06@users.noreply.github.com>
2025-12-04 22:54:37 -0500

    arreglo jpauserdetailsservices

--- src/main/java/com/SOA/alumno_api/auth/service/JpaUserDetailsService.java
+++ src/main/java/com/SOA/alumno_api/auth/service/JpaUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.soa.alumno_api.auth.service;
 
+import com.soa.alumno_api.auth.entity.Usuario;
 import com.soa.alumno_api.auth.repo.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.*;
@@ -12,7 +13,14 @@ public class JpaUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repo.findByUsername(username)
+
+        Usuario u = repo.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("No existe el usuario"));
+
+        return User.builder()
+                .username(u.getUsername())
+                .password(u.getPassword())
+                .roles(u.getRole().name()) // genera ROLE_ADMIN o ROLE_SECRETARIA
+                .build();
     }
 }
